da9be61ace51fca44847cff833235f4e07a85ed3
Fabiano Romero de Azevedo <fazevedo.dev@gmail.com>
2014-02-03 13:30:57 -0200

    Added CSS shadowing, not working on IE though.

--- templates/index.tpl
+++ templates/index.tpl
@@ -2,16 +2,21 @@
 <html>
     {include file="header.tpl"}
     <body>
-        {include file="menu.tpl"
-            menu_item_list=$menu_item_list
-            title=$title}
-        <div class="clear"></div>
-        <div class="container_24">
-            <div class="grid_24">
-                <div class="content">
-                    Body
+        <div class="ieBlock"></div>
+        <div class="container_24" id="box-shadow">
+            {include file="menu.tpl"
+                menu_item_list=$menu_item_list
+                title=$title}
+            <div class="clear"></div>
+            <div class="container_24">
+                <div class="grid_24">
+                    <div class="content">
+                        Body
+                    </div>
                 </div>
             </div>
+        </div>
+        <div class="container_24">
             {include file="footer.tpl"}
         </div>
     </body>
--- templates_c/749422d4cfc3eb5677cf499730392b6accd4d1c7.file.index.tpl.php
+++ templates_c/749422d4cfc3eb5677cf499730392b6accd4d1c7.file.index.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.16, created on 2014-02-03 15:08:23
+<?php /* Smarty version Smarty-3.1.16, created on 2014-02-03 16:07:19
          compiled from ".\templates\index.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:765152d7fa0517d673-50060967%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '749422d4cfc3eb5677cf499730392b6accd4d1c7' => 
     array (
       0 => '.\\templates\\index.tpl',
-      1 => 1391436498,
+      1 => 1391440034,
       2 => 'file',
     ),
   ),
@@ -29,15 +29,20 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     <?php echo $_smarty_tpl->getSubTemplate ("header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
 
     <body>
-        <?php echo $_smarty_tpl->getSubTemplate ("menu.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array('menu_item_list'=>$_smarty_tpl->tpl_vars['menu_item_list']->value,'title'=>$_smarty_tpl->tpl_vars['title']->value), 0);?>
+        <div class="ieBlock"></div>
+        <div class="container_24" id="box-shadow">
+            <?php echo $_smarty_tpl->getSubTemplate ("menu.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array('menu_item_list'=>$_smarty_tpl->tpl_vars['menu_item_list']->value,'title'=>$_smarty_tpl->tpl_vars['title']->value), 0);?>
 
-        <div class="clear"></div>
-        <div class="container_24">
-            <div class="grid_24">
-                <div class="content">
-                    Body
+            <div class="clear"></div>
+            <div class="container_24">
+                <div class="grid_24">
+                    <div class="content">
+                        Body
+                    </div>
                 </div>
             </div>
+        </div>
+        <div class="container_24">
             <?php echo $_smarty_tpl->getSubTemplate ("footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
 
         </div>
